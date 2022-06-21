57138af9f5eb2f5ea0bc63eaac91413f97897a4e
jorge <goncalves2917@gmail.com>
2022-06-21 17:41:58 -0300

    adicionando info a base de conhecimento

--- chatbot.pl
+++ chatbot.pl
@@ -12,6 +12,27 @@ info('covid','tratamento','Cientistas do mundo todo estão trabalhando para enco
                            A Dexametasona é um corticosteroide que pode ajudar a reduzir o tempo de ventilação e salvar as vidas de pacientes em situação mais grave.
                            A OMS não recomenda a automedicação, incluindo ingestão de antibióticos, como prevenção ou cura da COVID-19.').
 
+info('dengue','prevencao' , 'A melhor forma de prevenção da dengue é evitar a proliferação do mosquito Aedes Aegypti,
+                            eliminando água armazenada que podem se tornar possíveis criadouros.
+                            Utilize repelente. Cubra a maior parte do corpo com roupas claras quando possível. 
+                            Coloque telas em janelas e portas. O mosquito possui hábitos diurnos,
+                            sobretudo ao amanhecer e ao entardecer.').
+
+info('dengue','sintomas','Os sintomas são febre alta, erupções cutâneas e dores musculares e articulares. 
+                          Em casos graves, há hemorragia intensa e choque hemorrágico (quando uma pessoa perde mais de 20% do sangue ou fluido corporal), o que pode ser fatal.').
+
+info('dengue','tratamento','O tratamento é feito por meio do uso de fluidos e de medicamentos para a dor.
+							O tratamento inclui ingestão de líquidos e analgésicos. Os casos graves exigem cuidados hospitalares.').
+
+info('gripe','prevencao','Evitar mudanças bruscas de temperatura, lavar as mãos com freqência, evitar tocar os olhos a boca e o nariz,
+                          ter uma alimentação balanceada e práticar exercícios, são medidas que evitam a gripe comum.').
+
+info('gripe','sintomas','Os sintomas incluem febre, calafrios, dores musculares, tosse, congestão, coriza, dores de cabeça e fadiga.').
+
+info('gripe','tratamento','A gripe é tratada principalmente com repouso e ingestão de líquidos para permitir que o corpo combata a infecção por conta própria.
+                           Analgésicos anti-inflamatórios vendidos sem prescrição médica podem ajudar com os sintomas.
+                           Uma vacina anual pode ajudar a prevenir a gripe e limitar suas complicações.').
+
 sim('sim').
 nao('nao').
 
@@ -39,4 +60,4 @@ resposta(X, Doenca, R):-
     		(sim(X) -> 
     		write('Sobre a '), write(Doenca), write(', o que deseja saber?'), nl,
     		read(Info),
-    		consulta(Doenca, Info, R); nao(X) -> final(Doenca)).
+    		consulta(Doenca, Info, R); nao(X) -> write('Até uma próxima pergunta!!'), nl, final(Doenca)).
